736244ac13778a1912672a48e79847548818837b
yuzhen <caiyuzhen00@gmail.com>
2022-09-30 22:53:52 +0800

    init

---
+++ src/components/Test.tsx
@@ -0,0 +1,14 @@
+import React, { FC, ReactElement } from 'react'
+import './Test.less'
+
+
+const Test:FC = ():ReactElement => {
+  return (
+	<div id="wrap">
+		<div className="inner">啦啦啦</div>
+	</div>
+  )
+}
+
+
+export default Test
