d697001715202bca7c07e9fbc9aca6016225c3d1
yuzhen <caiyuzhen00@gmail.com>
2022-10-01 13:52:08 +0800

    udpdate learning

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -6,9 +6,21 @@ import './Test.less'
 const Test:FC = ():ReactElement => {
   return (
 	<>
-		<div id='wrap'>
-			<div className='inner'>🌞Hey</div>
+		<div id='wrap-one'>
+			{/* 两个元素需要用同一个样式的话，就需要用到less的【混合】，避免重复代码 */}
+			<div className='inner'>
+				<div className="inner-text">🌞Hey</div>
+			</div>
 		</div>
+
+		<div id='wrap-two'>
+			<div className='inner2'>
+				{/* 两个元素需要用同一个样式的话，就需要用到less的【混合】，避免重复代码 */}
+				<div className="inner-text2">🌞Hey</div>
+			</div>
+		</div>
+
+		
 	</>
   )
 }
