4471ce4c2a887972ad5a4a566c0ed447b13c26de
Zen <caiyuzhen00@gmail.com>
2022-10-03 00:14:08 +0800

    bug fix

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useState } from 'react'
+import React, { FC, ReactElement, useEffect, useState } from 'react'
 // import styles from './Test.less'
 import './Test.less'
 
@@ -14,20 +14,36 @@ const Test:FC = ():ReactElement => {
 
 
 	//状态开关控制颜色
-	const [open, setOpen] = useState<boolean>(false)
-	// localStorage.setItem("dark", '#000000')
+	const [openDark, setOpenDark] = useState<boolean>(false)
 
+	
+	//🔥文档加载完毕时候，获取【上一次记录的色值】以及【上一次记录的状态！】
+	useEffect(()=>{
+		// ⚡️⚡️初次加载时，获取本地的【主题色值】
+		document.body.style.backgroundColor = localStorage.getItem("dark") as string || localStorage.getItem("light") as string
+		// ⚡️⚡️获取本地的 openDark 状态, 用于控制开关来切换主题色, 看上一次是开启还是关闭的【状态】！
+		setOpenDark(JSON.parse(localStorage.getItem("openDark") as string))
+	},[]) 
+
+
+
+	// 点击切换主题色的方法
 	const changeColor= () :void => {
-		setOpen(!open)
-		//https://juejin.cn/post/6844904176179216397
-		if(open){
-			localStorage.setItem("light", '#FFFFFF')//开关关闭了，所以把浅色模式保存到本地
-			document.body.style.backgroundColor = localStorage.getItem("light") as string//把浅色模式设置为背景色
+
+		setOpenDark(!openDark)
+
+		if(openDark){
+			localStorage.setItem("light", themeColor.light)//开关关闭了，所以把浅色模式保存到本地，先做这步
+			localStorage.setItem("openDark", JSON.stringify(false))//设置本地的 openDark 状态
+			document.body.style.backgroundColor = (localStorage.getItem("light")) as string//然后设置整个网页为浅色背景
+			localStorage.removeItem("dark");//然后清除本地的深色模式
+
 		}else{
-			//一开始执行这个，因为已经点击了，所以把深色模式保存到本地
-			localStorage.setItem("dark", '#000000')//开关打开了，所以把深色模式保存到本地
-			document.body.style.backgroundColor = (localStorage.getItem("dark")) as string//把深色模式设置为背景色
-			console.log(localStorage.getItem("dark") )
+			localStorage.setItem("dark", themeColor.dark)//开关打开了，所以把深色模式保存到本地，先做这步
+			localStorage.setItem("openDark", JSON.stringify(true))//设置本地的 openDark 状态
+			document.body.style.backgroundColor = (localStorage.getItem("dark")) as string//然后设置网页为深色背景			
+			localStorage.removeItem("light");//然后清除本地的浅色模式
+
 		}
 
 	}
@@ -47,7 +63,7 @@ const Test:FC = ():ReactElement => {
 				<div className="inner-text2">🌞Hey</div>
 			</div>
 		</div>
-
+·
 		<div className="triangle">
 			<div className="triangle-border"></div>
 		</div>
@@ -60,10 +76,10 @@ const Test:FC = ():ReactElement => {
 			<div className="square-three">3</div>
 		</div>
 
-		<div className="change-color">
+		<div>
 			<div 
 				id='thisBox'
-				className={open ? 'changeBox_dark' : 'changeBox_light'}
+				className={openDark ? 'changeBox_dark' : 'changeBox_light'}
 				// style={{backgroundColor:red}}
 				>需要改变我的颜色</div>
 			<button onClick={ changeColor }>点我改变主题色</button>
